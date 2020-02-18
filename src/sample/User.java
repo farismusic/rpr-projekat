c93a53a98d6eac90aab357d4fd1f8d25ef5a7bca
farismusic <44246682+farismusic@users.noreply.github.com>
2020-02-18 23:59:28 +0100

    Kreiran GUI za Login i Registraciju.

---
+++ src/sample/User.java
@@ -0,0 +1,71 @@
+package sample;
+
+public class User {
+
+    private int id;
+    private String username, name, lastName, email, password;
+
+    public User(int id, String username, String name, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public User setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public User setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public User setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public User setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
