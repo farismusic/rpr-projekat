9fe4f0b0aabddc34a9a1197ffffd644b3f73801c
farismusic <44246682+farismusic@users.noreply.github.com>
2020-02-19 01:04:31 +0100

    Dodana validacija na formu za registrovanje

--- src/sample/BibliotekaDAO.java
+++ src/sample/BibliotekaDAO.java
@@ -1,22 +1,18 @@
 package sample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.Scanner;
 
 public class BibliotekaDAO {
 
     private static BibliotekaDAO instance;
     private Connection connection;
-    private PreparedStatement addUserQuery;
-
-    public static BibliotekaDAO getInstance() {
-        if (instance == null) instance = new BibliotekaDAO();
-        return instance;
-    }
+    private PreparedStatement addUserQuery, addAdminQuery;
 
     private BibliotekaDAO(){
+
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:baza.db");
         } catch (SQLException e) {
@@ -26,13 +22,64 @@ public class BibliotekaDAO {
         try {
             addUserQuery = connection.prepareStatement("INSERT INTO user VALUES (?, ?, ?, ?, ?)");
         } catch (SQLException e) {
+            regenerateDB();
+            try {
+                addUserQuery = connection.prepareStatement("INSERT INTO user VALUES (?, ?, ?, ?, ?)");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        try {
+            addAdminQuery = connection.prepareStatement("INSERT INTO admin VALUES (?, ?, ?, ?, ?)");
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
 
     }
 
+    public static BibliotekaDAO getInstance() {
+        if (instance == null) instance = new BibliotekaDAO();
+        return instance;
+    }
 
+    public static void removeInstance() {
+        if (instance == null) return;
+        instance.close();
+        instance = null;
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void regenerateDB() {
+        Scanner ulaz = null;
+        try {
+            ulaz = new Scanner(new FileInputStream("baza.db.sql"));
+            String sqlUpit = "";
+            while (ulaz.hasNext()) {
+                sqlUpit += ulaz.nextLine();
+                if ( sqlUpit.charAt( sqlUpit.length()-1 ) == ';') {
+                    try {
+                        Statement stmt = connection.createStatement();
+                        stmt.execute(sqlUpit);
+                        sqlUpit = "";
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            ulaz.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void addUser(User user){
         try {
@@ -43,6 +90,20 @@ public class BibliotekaDAO {
             addUserQuery.setString(5, user.getPassword());
 
             addUserQuery.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addAdmin(User user){
+        try {
+            addAdminQuery.setString(1, user.getUsername());
+            addAdminQuery.setString(2, user.getName());
+            addAdminQuery.setString(3, user.getLastName());
+            addAdminQuery.setString(4, user.getEmail());
+            addAdminQuery.setString(5, user.getPassword());
+
+            addAdminQuery.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/sample/RegisterController.java
+++ src/sample/RegisterController.java
@@ -13,8 +13,8 @@ public class RegisterController {
     public PasswordField fieldPassword;
     public TextField fieldEmail;
     public TextField fieldUsername;
-    public TextField fieldPrezime;
-    public TextField fieldIme;
+    public TextField fieldLastName;
+    public TextField fieldName;
     private BibliotekaDAO baza;
 
     public RegisterController() {
@@ -23,6 +23,71 @@ public class RegisterController {
 
 
     public void registruj(ActionEvent actionEvent) {
-        baza.addUser(new User(fieldUsername.getText(), fieldIme.getText(), fieldPrezime.getText(), fieldEmail.getText(), fieldPassword.getText()));
+
+        boolean sveOk = true;
+
+        if (fieldUsername.getText().trim().isEmpty()) {
+            fieldUsername.getStyleClass().removeAll("poljeIspravno");
+            fieldUsername.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        } else {
+            fieldUsername.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldUsername.getStyleClass().add("poljeIspravno");
+        }
+
+        if (fieldName.getText().trim().isEmpty()) {
+            fieldName.getStyleClass().removeAll("poljeIspravno");
+            fieldName.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        } else {
+            fieldName.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldName.getStyleClass().add("poljeIspravno");
+        }
+
+        if (fieldLastName.getText().trim().isEmpty()) {
+            fieldLastName.getStyleClass().removeAll("poljeIspravno");
+            fieldLastName.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        } else {
+            fieldLastName.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldLastName.getStyleClass().add("poljeIspravno");
+        }
+
+        if (fieldEmail.getText().trim().isEmpty()) {
+            fieldEmail.getStyleClass().removeAll("poljeIspravno");
+            fieldEmail.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        } else {
+            fieldEmail.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldEmail.getStyleClass().add("poljeIspravno");
+        }
+
+        if (fieldPassword.getText().trim().isEmpty()) {
+            fieldPassword.getStyleClass().removeAll("poljeIspravno");
+            fieldPassword.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        } else {
+            fieldPassword.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldPassword.getStyleClass().add("poljeIspravno");
+        }
+
+        if (fieldPasswordRepeat.getText().trim().isEmpty()) {
+            fieldPasswordRepeat.getStyleClass().removeAll("poljeIspravno");
+            fieldPasswordRepeat.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        } else {
+            fieldPasswordRepeat.getStyleClass().removeAll("poljeNijeIspravno");
+            fieldPasswordRepeat.getStyleClass().add("poljeIspravno");
+        }
+
+        if(!fieldPassword.getText().equals(fieldPasswordRepeat.getText())){
+            fieldPasswordRepeat.getStyleClass().removeAll("poljeIspravno");
+            fieldPasswordRepeat.getStyleClass().add("poljeNijeIspravno");
+            sveOk = false;
+        }
+
+        if(!sveOk) return;
+
+        baza.addUser(new User(fieldUsername.getText(), fieldName.getText(), fieldLastName.getText(), fieldEmail.getText(), fieldPassword.getText()));
     }
 }
