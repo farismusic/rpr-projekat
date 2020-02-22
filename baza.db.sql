aa552d9166fdf9703c740904f392d3de9d1438e2
farismusic <44246682+farismusic@users.noreply.github.com>
2020-02-22 11:29:04 +0100

    kreirana tabela iznajmljivanja i klasa iznajmljivanja implementirana

--- baza.db.sql
+++ baza.db.sql
@@ -1,4 +1,14 @@
 BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "rentings" (
+	"id"	INTEGER,
+	"renter"	TEXT,
+	"book"	INTEGER,
+	"begin"	TEXT,
+	"end"	TEXT,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("renter") REFERENCES "users"("username"),
+	FOREIGN KEY("book") REFERENCES "books"("id")
+);
 CREATE TABLE IF NOT EXISTS "books" (
 	"id"	INTEGER,
 	"naziv"	TEXT,
@@ -24,6 +34,8 @@ CREATE TABLE IF NOT EXISTS "users" (
 	"password"	TEXT,
 	PRIMARY KEY("username")
 );
+INSERT INTO "books" VALUES (1,'Derviš i smrt','Meša Selimović','psihološka drama',431,50);
 INSERT INTO "admins" VALUES ('farismusic','Faris','Mušić','fmusic2@etf.unsa.ba','fmusic123');
 INSERT INTO "users" VALUES ('sturko1','Sajra','Turko','sturko1@etf.unsa.ba','nekaidezivot99');
+INSERT INTO "users" VALUES ('aturkusic1','Arslan','Turkušić','aturkusic1@etf.unsa.ba','12345678');
 COMMIT;
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main extends Application {
 
     @Override
@@ -15,6 +18,8 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 250, 225));
         primaryStage.setResizable(false);
         primaryStage.show();
+        LocalDateTime datum = LocalDateTime.now();
+        System.out.println(datum.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")));
     }
 
 
